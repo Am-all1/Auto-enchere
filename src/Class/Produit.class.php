f5986a00b561ff2c38667122b8d91be0a07b8111
Aamndine <amandine.l@live.fr>
2022-06-15 17:54:27 +0200

    ajout classe Produit

---
+++ src/Class/Produit.class.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Classe produit
+ */
+
+class User
+{
+
+    /* Propriétés */
+    public string $id;
+    public string $prix_de_reserve; 
+    public int $fin_enchere;
+    public string $date;
+    public string $modele;
+    public string $puissance; 
+    public int $annee;
+    public string $description;
+    
+    /**
+     * Constructeur
+     */
+    public function __construct(string $id, string $prix_de_reserve,string $fin_enchere,string $date, string $modele, string $puissance, string $annee,string $description)
+    {
+    $this-> id =$id;
+    $this-> prix_de_reserve =$prix_de_reserve; 
+    $this-> fin_enchere =$fin_enchere;
+    $this-> date =$date;
+    $this->modèle =$modele;
+    $this-> puissance =$puissance; 
+    $this-> annee =$annee;
+    $this-> description =$description;
+    }
+
+};
